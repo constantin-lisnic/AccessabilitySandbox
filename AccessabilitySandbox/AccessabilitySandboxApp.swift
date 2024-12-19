ac32db4569371c36c19f38c91b643a62662a6ed9
Constantin Lisnic <constantin@lisnic.dev>
2024-12-19 13:54:14 +0000

    Initial Commit

---
+++ AccessabilitySandbox/AccessabilitySandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessabilitySandboxApp.swift
+//  AccessabilitySandbox
+//
+//  Created by Constantin Lisnic on 19/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct AccessabilitySandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AccessabilitySandboxTests/AccessabilitySandboxTests.swift
@@ -0,0 +1,17 @@
+//
+//  AccessabilitySandboxTests.swift
+//  AccessabilitySandboxTests
+//
+//  Created by Constantin Lisnic on 19/12/2024.
+//
+
+import Testing
+@testable import AccessabilitySandbox
+
+struct AccessabilitySandboxTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
